47db0ad5c807fc6c1af818d0369c8d367aad4ac0
Johnny Santos <johnnyadsantos@gmail.com>
2018-05-20 21:36:06 -0300

    Progress with the download of ebooks

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 from requests_html import HTMLSession
 from sys import exit, argv
 
+
 def show_help():
     print("""Packt E-Book Importer: Version 0.0.1
     -h | --help \tShow this help message
@@ -16,22 +17,34 @@ def show_help():
     <cookies>\tCookies from a logged session in your browser
 """)
 
+
 def validate_args():
     if len(argv) != 2 or (argv[1] == "-h" or argv[1] == "--help"):
         show_help()
         exit(1)
-    
+
     return argv[1]
 
+
 def fetch_books(cookie):
-    # session = HTMLSession()
+    session = HTMLSession()
 
-    values = (item.trim().split('=') for item in cookie.split(';'))
+    cookie_values = (item.split('=') for item in cookie.split(';'))
 
-    for value in values:
-        print(value)
+    for cookie_value in cookie_values:
+        session.cookies.set(cookie_value[0], cookie_value[1])
 
-    # session.cookies.set()
+    response = session.get("https://www.packtpub.com/account/my-ebooks")
+    ebooks = response.html.find(".product-line")
+    i = 0
+    for ebook in ebooks:
+        i += 1
+        print("Importing ebook {%r} {%i}/{%i}".format(ebook.attrs["title"], i,
+                                                      len(ebooks)))
+        
+        mobi_link = ebook.find("a[href~=/mobi]")
+        ebook_file = session.get(mobi_link.attrs["href"])
+        #TODO: Download mobi file
 
 
 def main():
